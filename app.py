7b5258114308cddd2251e409657186915027a958
Nicholas Goodman (SFC) <65569681+sfc-gh-ngoodman@users.noreply.github.com>
2022-06-03 12:33:19 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,27 @@
-import streamlit as st
 
+import streamlit as st
+import snowflake.connector
 
 st.title("Hello World!")
+
+# Initialize connection.
+# Uses st.experimental_singleton to only run once.
+@st.experimental_singleton
+def init_connection():
+    return snowflake.connector.connect(**st.secrets["snowflake"])
+
+conn = init_connection()
+
+# Perform query.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+rows = run_query("SELECT current_account();")
+
+# Print results.
+for row in rows:
+    st.write(f"Result : {row[0]}")
